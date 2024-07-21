e62f8d6a62ad4885617d2c24ceeb0f4a64ff6c9b
saivamsisis <saivamsisistu@gmail.com>
2024-07-21 23:04:34 +0530

    Update DAY3.py

--- DAY3.py
+++ DAY3.py
@@ -1,5 +1,8 @@
 #CrackYourPlacement
-'class Solution:
+'''TO DAY I HAVE DONE TWO QUESTIONS FROM ARRAYS:
+WHICH ARE:
+1)REMOVE DUPLICATES PROBLEM:'''
+class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         y=nums[0]
         l=[y]
@@ -10,6 +13,25 @@
         for i in range(len(l)):
             nums[i]=l[i]
         
-        return len(nums[:len(l)])'
+        return len(nums[:len(l)])
+#2)MOVE ZEROS:
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        c=0
+        l=[]
+        for i in nums:
+            if i!=0:
+                l.append(i)
+            else:
+                c=c+1
+        for i in range(c):
+            l.append(0)
+        for i in range(len(nums)):
+            nums[i]=l[i]
+
+
             
         `
