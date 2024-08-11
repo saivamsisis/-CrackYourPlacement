77c5335a47fc0e61422a81efcd762dceb744df4b
saivamsisis <saivamsisistu@gmail.com>
2024-08-11 21:46:09 +0530

    Create Day23.py

---
+++ Day23.py
@@ -0,0 +1,27 @@
+#)Integer to Roman
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        roman_dict = {1: "I",5: "V",4: "IV",10: "X",   9: "IX",50: "L",   40: "XL",100: "C",  90: "XC",500: "D",  400: "CD",1000: "M", 900: "CM"}
+        out = ""
+        for n in [1000,900,500,400,100,90,50,40,10,9,5,4,1]:
+            while num >= n:
+                out += roman_dict[n]
+                num -= n
+        return out
+#). Longest Common Prefix
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        pref = strs[0]
+        pref_len = len(pref)
+
+        for s in strs[1:]:
+            while pref != s[0:pref_len]:
+                pref_len -= 1
+                if pref_len == 0:
+                    return ""
+                
+                pref = pref[0:pref_len]
+        
+        return pref
+
+            
